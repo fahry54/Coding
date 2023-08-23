f47377cc14fa52922a18431f158ac1051b7be220
Achmad Fahry <achmad.fahry@gmail.com>
2023-08-23 19:00:48 +0700

    machine learning dan bisnis case

---
+++ porto_busines_decision_report/Feature_Columns_dan_Target.py
@@ -0,0 +1,20 @@
+import pandas as pd
+df = pd.read_csv('data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Year_First_Transaction'] = df['First_Transaction'].dt.year
+df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
+df.loc[df['Last_Transaction'] <= '2018-08-01', 'is_churn'] = True 
+df.loc[df['Last_Transaction'] > '2018-08-01', 'is_churn'] = False 
+
+# Feature column: Year_Diff
+df['Year_Diff'] = df['Year_Last_Transaction'] - df['Year_First_Transaction']
+
+# Nama-nama feature columns
+feature_columns = ['Average_Transaction_Amount', 'Count_Transaction', 'Year_Diff']
+
+# Features variable
+X = df[feature_columns] 
+
+# Target variable
+y = df['is_churn'] 
---
+++ porto_busines_decision_report/accuracy_Precision_dan_Recall.py
@@ -0,0 +1,32 @@
+import pandas as pd
+df = pd.read_csv('data_retail.csv', sep=';')
+df['First_Transaction'] = pd.to_datetime(df['First_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Last_Transaction'] = pd.to_datetime(df['Last_Transaction']/1000, unit='s', origin='1970-01-01')
+df['Year_First_Transaction'] = df['First_Transaction'].dt.year
+df['Year_Last_Transaction'] = df['Last_Transaction'].dt.year
+df.loc[df['Last_Transaction'] <= '2018-08-01', 'is_churn'] = True 
+df.loc[df['Last_Transaction'] > '2018-08-01', 'is_churn'] = False 
+
+df['Year_Diff']=df['Year_Last_Transaction']-df['Year_First_Transaction']
+feature_columns = ['Average_Transaction_Amount', 'Count_Transaction', 'Year_Diff']
+
+X = df[feature_columns] 
+y = df['is_churn'] 
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+y_pred = logreg.predict(X_test)
+cnf_matrix = confusion_matrix(y_test, y_pred)
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+#Menghitung Accuracy, Precision, dan Recall
+print('Accuracy :', accuracy_score(y_test, y_pred))
+print('Precision:', precision_score(y_test, y_pred, average='micro'))
+print('Recall   :',recall_score(y_test, y_pred, average='micro'))
